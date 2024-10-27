c5e722eb1fc1c80fbf40f8105de65a4e8954a85d
Shrinil <shrinil1997@gmail.com>
2024-10-27 11:42:56 +0530

    First Commit

---
+++ src/main/java/com/LMS/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.LMS.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/")
+    String index() {
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        return "index";
+    }
+}
